b38e48bb98b90ae7cde2f2fc78b86984bcf110e6
Austin Myers <austin.myers@mail.missouri.edu>
2019-11-30 23:36:08 -0600

    Uhh whereever im at

--- index.js
+++ index.js
@@ -15,6 +15,20 @@ server.listen(4000, () => {
 	console.log('Listening on port 4000');
 });
 
+const joinRoom = (authState, displayName, roomCode) => {firebase.database().ref('games').orderByChild('code').equalTo(roomCode).on("value", (snapshot) => {
+	let id
+	if(snapshot && snapshot.val()){
+		snapshot.forEach(function(data) {
+			id = data.key
+		})
+
+		firebase.database().ref('games/' + id + '/connected_players/' + authState.uid).set({name: displayName})
+		return id
+	}
+})
+};
+
+const loginAnonymously =  () => firebase.auth().signInAnonymously()
 
 io.on('connection', (socket) => {
 	console.log('Successful connection');
@@ -37,9 +51,14 @@ io.on('connection', (socket) => {
 
 	socket.on('initialize', () => {
 
-		credentials = loginAnonymously();
-		console.log(credentials);
-		joinRoom(credentials, socket.username, socket.roomcode);
+		if(socket.username && socket.roomcode){
+			credentials = loginAnonymously();
+			console.log(credentials);
+			joinRoom(credentials, socket.username, socket.roomcode);
+		}
+		else{
+			console.log('They werent set yet')
+		}
 			
 	});
 });
@@ -48,18 +67,3 @@ io.on('disconnect', (socket) => {
 	console.log('Client disconnected');
 });
 
-
-const joinRoom = (authState, displayName, roomCode) => {firebase.database().ref('games').orderByChild('code').equalTo(roomCode).on("value", (snapshot) => {
-	let id
-	if(snapshot && snapshot.val()){
-		snapshot.forEach(function(data) {
-			id = data.key
-		})
-
-		firebase.database().ref('games/' + id + '/connected_players/' + authState.uid).set({name: displayName})
-		return id
-	}
-})
-};
-
-const loginAnonymously =  () => firebase.auth().signInAnonymously()
