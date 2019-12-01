a7821a709ab925e94c4c086ba13003edfa7828e1
Austin Myers <austin.myers@mail.missouri.edu>
2019-12-01 00:18:55 -0600

    hello cold world

--- index.js
+++ index.js
@@ -15,20 +15,6 @@ server.listen(4000, () => {
 	console.log('Listening on port 4000');
 });
 
-const joinRoom = (authState, displayName, roomCode) => {firebase.database().ref('games').orderByChild('code').equalTo(roomCode).on("value", (snapshot) => {
-	let id
-	if(snapshot && snapshot.val()){
-		snapshot.forEach(function(data) {
-			id = data.key
-		})
-
-		firebase.database().ref('games/' + id + '/connected_players/' + authState.uid).set({name: displayName})
-		return id
-	}
-})
-};
-
-const loginAnonymously =  () => firebase.auth().signInAnonymously()
 
 io.on('connection', (socket) => {
 	console.log('Successful connection');
@@ -49,21 +35,47 @@ io.on('connection', (socket) => {
 	socket.on('send move', (direction) => {
 	});
 
-	socket.on('initialize', () => {
-
-		if(socket.username && socket.roomcode){
-			credentials = loginAnonymously();
-			console.log(credentials);
-			joinRoom(credentials, socket.username, socket.roomcode);
-		}
-		else{
-			console.log('They werent set yet')
-		}
-			
-	});
+	socket.on('initialize',
 });
 
 io.on('disconnect', (socket) => {
 	console.log('Client disconnected');
 });
 
+
+const joinRoom = (authState, displayName, roomCode) => {firebase.database().ref('games').orderByChild('code').equalTo(roomCode).on("value", (snapshot) => {
+	let id
+	if(snapshot && snapshot.val()){
+		snapshot.forEach(function(data) {
+			id = data.key
+		})
+
+		firebase.database().ref('games/' + id + '/connected_players/' + authState.uid).set({name: displayName})
+		return id
+	}
+})
+};
+
+const loginAnonymously =  () => firebase.auth().signInAnonymously()
+
+
+//For waiting for variables to be set
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function initializeFn() {
+
+	
+	while(!(socket.username && socket.roomcode)){
+		console.log('Not set yet.')
+		await sleep(1000);
+	}
+
+	credentials = loginAnonymously();
+	console.log(credentials);
+	joinRoom(credentials, socket.username, socket.roomcode);
+		
+
+}
